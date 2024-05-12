419e81f5a1da5f7f97e021b8b0f00a85fe62e357
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2024-05-12 21:00:04 +0300

    0000

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -12,19 +12,15 @@ export default function Home() {
       <Hero />
 
       <div className="mt-12 pt-8 mb-24 border-t-1 border-gray-100">
-        <h1 className="text-center text-xl text-red-600 mb-8">
+        <h1 className="text-center text-xl text-red-600 mb-8 font-bold">
           {t("chooseLocation")}
         </h1>
         <h1 className="text-center text-4xl font-bold text-gray-600 dark:text-gray-200">
           {t("ourBranches")}
         </h1>
 
-        {/* <div className="p-4 pt-0 mt-0 mx-auto text-center0 border-0 flex flex-row justify-around"> */}
-          <Branches />
-          {/* <div className="w-full w-6xl mx-auto pb-4 py-5">
-        <h1>some text</h1>
-      </div> */}
-        {/* </div> */}
+        <Branches />
+      
       </div>
 
       <div className="mt-24 mb-24">
--- app/components/VideoPage.tsx
+++ app/components/VideoPage.tsx
@@ -2,7 +2,7 @@
 
 export default function VideoPage() {
   return (
-    <video className="w-full h-full" controls muted autoPlay poster="/images/cover.png" preload="true">
+    <video className="w-full h-full" controls muted autoPlay loop poster="/images/cover.png" preload="true">
       <source src="/video_2024-05-09_20-52-56.mp4" type="video/mp4" />
       <track
         src="/video_2024-05-09_20-52-56.mp4"
